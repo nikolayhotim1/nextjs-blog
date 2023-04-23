c3770774806fd5c9ca3b2074bf7388aa8629597e
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-04-23 16:36:48 +0300

    feature: improve layout component and home page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,6 @@
-import Link from 'next/link'
-import Head from 'next/head'
 import Layout from '../../components/layout'
+import Head from 'next/head'
+import Link from 'next/link'
 
 export default function FirstPost() {
 	return (
