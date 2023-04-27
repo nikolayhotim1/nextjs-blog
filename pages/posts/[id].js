cb6120a90b396985a3f50fd38dc38b62840781f9
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-04-27 19:10:24 +0300

    feature: add posts links and some styles to app

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,7 @@
+import utilStyles from '../../styles/utils.module.css'
 import Layout from '../../components/layout'
+import Date from '../../components/date'
+import Head from 'next/head'
 import { getAllPostsIds, getPostData } from '../../lib/posts'
 
 export async function getStaticPaths() {
@@ -22,13 +25,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
 	return (
 		<Layout>
-			{postData.title}
-			<br />
-			{postData.id}
-			<br />
-			{postData.date}
-			<br />
-			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<article>
+				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
 		</Layout>
 	)
 }
