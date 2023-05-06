ab0f1e273bc5d51a26a1e218e90a6e6f45a4c410
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-05-06 13:29:51 +0300

    refactor: migrate app to ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,23 +4,26 @@ import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
 
-const postsDirectory = path.join(process.cwd(), 'data')
+const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
-	// Get file names under /data
+	// Get file names under /posts
 	const fileNames = fs.readdirSync(postsDirectory)
 	const allPostsData = fileNames.map(fileName => {
 		// Remove ".md" from file name to get id
 		const id = fileName.replace(/\.md$/, '')
+
 		// Read markdown file as string
 		const fullPath = path.join(postsDirectory, fileName)
 		const fileContents = fs.readFileSync(fullPath, 'utf8')
+
 		// Use gray-matter to parse the post metadata section
 		const matterResult = matter(fileContents)
+
 		// Combine the data with the id
 		return {
 			id,
-			...matterResult.data
+			...(matterResult.data as { date: string; title: string })
 		}
 	})
 	// Sort posts by date
@@ -33,21 +36,8 @@ export function getSortedPostsData() {
 	})
 }
 
-export function getAllPostsIds() {
+export function getAllPostIds() {
 	const fileNames = fs.readdirSync(postsDirectory)
-	// Returns an array that looks like this:
-	// [
-	//   {
-	//     params: {
-	//       id: 'ssg-ssr'
-	//     }
-	//   },
-	//   {
-	//     params: {
-	//       id: 'pre-rendering'
-	//     }
-	//   }
-	// ]
 	return fileNames.map(fileName => {
 		return {
 			params: {
@@ -57,18 +47,21 @@ export function getAllPostsIds() {
 	})
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
 	const fullPath = path.join(postsDirectory, `${id}.md`)
 	const fileContents = fs.readFileSync(fullPath, 'utf8')
+
 	// Use gray-matter to parse the post metadata section
 	const matterResult = matter(fileContents)
+
 	// Use remark to convert markdown into HTML string
 	const processedContent = await remark().use(html).process(matterResult.content)
 	const contentHtml = processedContent.toString()
+
 	// Combine the data with the id and contentHtml
 	return {
 		id,
 		contentHtml,
-		...matterResult.data
+		...(matterResult.data as { date: string; title: string })
 	}
 }
